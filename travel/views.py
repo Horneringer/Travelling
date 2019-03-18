5aef8f3bb0d02af96bf1eccd813855b3dcb1bf2c
Horneringer <gureev9494@list.ru>
2019-03-18 19:48:07 +0300

    Созданы шаблонные тэги в html файлах для наследования

--- travel/views.py
+++ travel/views.py
@@ -7,6 +7,5 @@ from django.shortcuts import render
 # функция отображения страницы приветствия, на запрос возвращает http-response
 # функция render принимает запрос, шаблон(html файл) и словарь, где по ключу name подставляется соответствующее значение
 def home_view(request):
-    name = 'Зяблик'
     context = {'name': 'Щегол'}
     return render(request, 'home.html', context)
