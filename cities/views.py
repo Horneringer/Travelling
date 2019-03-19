3068368563de39fc7c7a5b2af3ea83f5f1317782
Horneringer <gureev9494@list.ru>
2019-03-20 02:39:16 +0300

    Создана функция отображения списка городов

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,14 @@
+# Dyenhb папки cities создаём папку templates/cities, в которой будет храниться файл home.html
+# c таким же содержимым, что и одноименный
 from django.shortcuts import render
 
-# Create your views here.
+# импортируем модель города
+from .models import City
+
+
+# функция отображения
+
+def home(request):
+    # запрос к базе  на получение всех записей; формируем html-ответ данными cities
+    cities = City.objects.all()
+    return render(request, 'cities/home.html', {'objects_list':cities})
