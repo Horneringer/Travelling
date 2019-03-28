8a51e37e50267d480a2c35f458926a4ae02759c5
Horneringer <gureev9494@list.ru>
2019-03-29 02:22:50 +0300

    Улучшено отображение полей формы маршрутов с помощью скриптов Select2

--- routes/forms.py
+++ routes/forms.py
@@ -3,17 +3,20 @@ from cities.models import City
 
 
 # форма для маршрутов; будет привязана к модели Города
+# добавляем классы из JS для работы select 2 в формах(одиночных и множественных)
+# классы находятся в скриптах в base.html
 class RouteForm(forms.Form):
     from_city = forms.ModelChoiceField(label='Откуда', queryset=City.objects.all(),
-                                       widget=forms.Select(attrs={'class': 'form-control', }))
+                                       widget=forms.Select(attrs={'class': 'form-control js-example-basic-single', }))
 
     to_city = forms.ModelChoiceField(label='Куда', queryset=City.objects.all(),
-                                     widget=forms.Select(attrs={'class': 'form-control', }))
+                                     widget=forms.Select(attrs={'class': 'form-control js-example-basic-single', }))
 
-    # поле 'через города'; осуществляется множественная выборка; поле будет необязательным для заполнения
+    # поле 'через города'; осуществляется множественная выборка; поле будет необязательным для заполнения;
+    # SelectMultiple позволяет выбирать несколько варантов для заполнения одного поля
 
     across_cities = forms.ModelMultipleChoiceField(label='Через города', queryset=City.objects.all(),
-                                                   widget=forms.Select(attrs={'class': 'form-control', }),
+                                                   widget=forms.SelectMultiple(attrs={'class': 'form-control js-example-basic-multiple', }),
                                                    required=False)
 
     traveling_time = forms.IntegerField(label='Время в пути', widget=forms.NumberInput(
