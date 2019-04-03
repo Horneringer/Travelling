3b88c773cf95bc87d3f523ab81c8d074d713bda5
Horneringer <gureev9494@list.ru>
2019-04-03 15:19:36 +0300

    Улучшена страница детализации маршрутов, добавлены поля в отображение админки для полездов

--- trains/admin.py
+++ trains/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Train
 
-# регистрируем приложение Train в админке
-admin.site.register(Train)
 
+# класс для кастомизации админки в приложения Поезда
+class TrainAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Train
+
+    # в list_display указываем все поля, которые нужны для отображения в админке
+    list_display = ('name', 'from_city', 'to_city', 'travel_time')
+    # метод позволяет на странице отображение редактировать поле
+    list_editable = ['travel_time']
+
+
+# регистрируем приложение Train в админке
+admin.site.register(Train, TrainAdmin)
