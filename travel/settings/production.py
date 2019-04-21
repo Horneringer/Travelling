a3633be54f65faf2e2ee9cf68529e8fe83cc3336
Horneringer <gureev9494@list.ru>
2019-04-22 02:26:54 +0300

    Disallowed host error

--- travel/settings/production.py
+++ travel/settings/production.py
@@ -37,9 +37,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['travelling-django.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 # Application definition
 
