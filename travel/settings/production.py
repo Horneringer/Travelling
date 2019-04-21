9cc14a1a69e7d1a3be39151f74ea34c1ae278551
Horneringer <gureev9494@list.ru>
2019-04-22 02:31:52 +0300

    Disallowed host error

--- travel/settings/production.py
+++ travel/settings/production.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['travelling-django.herokuapp.com']
 
 # Application definition
 
